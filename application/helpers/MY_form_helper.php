9cfdeadec9234ec11e6c7b1afd6804608de8105b
Phillip Jackson <unisys12@gmail.com>
2013-01-03 12:11:43 -0600

    Added helper extention for HTML5 Date input field

---
+++ application/helpers/MY_form_helper.php
@@ -0,0 +1,30 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * CodeIgniter HTML5 Form Helpers
+ *
+ * @package		CodeIgniter
+ * @subpackage	Helpers
+ * @category	Helpers
+ * @author 		Phillip Jackson
+ */
+
+// ------------------------------------------------------------------------
+
+/**
+ * Text Date Field
+ *
+ * @access	public
+ * @param	mixed
+ * @param	string
+ * @param	string
+ * @return	string
+ */
+if ( ! function_exists('form_date'))
+{
+	function form_date($data = '', $value = '', $extra = '')
+	{
+		$defaults = array('type' => 'date', 'name' => (( ! is_array($data)) ? $data : ''), 'value' => $value);
+
+		return "<input "._parse_form_attributes($data, $defaults).$extra." />";
+	}
+}
