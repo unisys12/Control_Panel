80f1e595668ee58ebf76af59c563276f7be1db7b
Phillip Jackson <unisys12@gmail.com>
2013-01-03 12:11:19 -0600

    Removed Date Helper
    Placed in incorrectly, extended properly in
    "application\helpers\My_form_helper.php"

--- system/helpers/form_helper.php
+++ system/helpers/form_helper.php
@@ -183,27 +183,6 @@ if ( ! function_exists('form_input'))
 // ------------------------------------------------------------------------
 
 /**
- * Text Date Field
- *
- * @access	public
- * @param	mixed
- * @param	string
- * @param	string
- * @return	string
- */
-if ( ! function_exists('form_date'))
-{
-	function form_date($data = '', $value = '', $extra = '')
-	{
-		$defaults = array('type' => 'date', 'name' => (( ! is_array($data)) ? $data : ''), 'value' => $value);
-
-		return "<input "._parse_form_attributes($data, $defaults).$extra." />";
-	}
-}
-
-// ------------------------------------------------------------------------
-
-/**
  * Password Field
  *
  * Identical to the input function but adds the "password" type
